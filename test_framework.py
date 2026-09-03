ed591d77a727860540c2038038a9482e430b0e9c
Test User <test@example.com>
2026-09-02 22:05:27 -0300

    Testando TestCase

--- test_framework.py
+++ test_framework.py
@@ -5,15 +5,6 @@ class TestCase:
 
     def run(self, result):
         result.test_started()
-        self.set_up()
-        try:
-            test_method = getattr(self, self.test_method_name)
-            test_method()
-        except AssertionError as e:
-            result.add_failure(self.test_method_name)
-        except Exception as e:
-            result.add_error(self.test_method_name)
-        self.tear_down()
 
     def set_up(self):
         pass
@@ -66,3 +57,37 @@ class TestResult:
             f"{str(len(self.failures))} {self.FAILURE_MSG}, "
             f"{str(len(self.errors))} {self.ERROR_MSG}"
         )
+
+
+class TestStub(TestCase):
+
+    def test_success(self):
+        assert True
+
+    def test_failure(self):
+        assert False
+
+    def test_error(self):
+        raise Exception
+
+
+class TestSpy(TestCase):
+
+    def __init__(self, name):
+        TestCase.__init__(self, name)
+        self.was_run = False
+        self.was_set_up = False
+        self.was_tear_down = False
+        self.log = ""
+
+    def set_up(self):
+        self.was_set_up = True
+        self.log += "set_up "
+
+    def test_method(self):
+        self.was_run = True
+        self.log += "test_method "
+
+    def tear_down(self):
+        self.was_tear_down = True
+        self.log += "tear_down"
