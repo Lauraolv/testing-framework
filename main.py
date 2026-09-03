286010ef1cfb8f23724a8cee13716864e34ebb56
Test User <test@example.com>
2026-09-02 22:13:45 -0300

    Executando Todos os Testes

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from test_framework import TestLoader, TestRunner
-from test_framework_test import TestLoader, TestLoaderTest, TestResult
+from test_framework import TestLoader, TestRunner, TestSuite
+from test_framework_test import TestCaseTest, TestLoader, TestLoaderTest, TestSuiteTest
 
 loader = TestLoader()
-suite = loader.make_suite(TestLoaderTest)
+test_case_suite = loader.make_suite(TestCaseTest)
+test_suite_suite = loader.make_suite(TestSuiteTest)
+test_load_suite = loader.make_suite(TestLoaderTest)
+
+suite = TestSuite()
+suite.add_test(test_case_suite)
+suite.add_test(test_suite_suite)
+suite.add_test(test_load_suite)
 
 runner = TestRunner()
 runner.run(suite)
